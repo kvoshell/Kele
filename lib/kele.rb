2e4f1786c3778614749978ce2f7151668cba995a
Kevin Voshell <kevinvoshell@gmail.com>
2017-05-02 01:11:33 -0500

    Retrieve mentor availability

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,7 +15,13 @@ class Kele
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token})
+    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
+  end
+
+  def get_mentor_availability(mentor_id)
+    @mentor_id = mentor_id
+    response = self.class.get("https://www.bloc.io/api/v1/mentors/#{@mentor_id}/student_availability", values: { "id" => @mentor_id }, headers: { "authorization" => @auth_token })
+    @availability = JSON.parse(response.body)
   end
 end
