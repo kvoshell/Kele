46dc31c467966a4433bfff4cfa79ab74d72da9bf
Kevin Voshell <kevinvoshell@gmail.com>
2017-05-02 02:41:50 -0500

    Add roadmap module, checkpoint lookup

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,21 +1,23 @@
 require 'httparty'
+require './lib/roadmap.rb'
 
 
 class Kele
   include HTTParty
+  include Roadmap
   base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
     @email = email
     @password = password
-    response = self.class.post('https://www.bloc.io/api/v1/sessions', body: { email: @email, password: @password })
+    response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { email: @email, password: @password })
     @auth_token = response["auth_token"]
     raise "Invalid token" if @auth_token == nil
     raise "Invalid email or password" unless response.code == 200
   end
 
   def get_me
-    response = self.class.get('https://www.bloc.io/api/v1/users/me', headers: { "authorization" => @auth_token })
+    response = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
     @user = JSON.parse(response.body)
   end
 
