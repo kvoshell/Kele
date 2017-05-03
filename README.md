1ebfd1beb89b7571da5353a6dc0c59311b1a9260
Kevin Voshell <kevinvoshell@gmail.com>
2017-05-03 01:58:57 -0500

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Kele
+
+Kele is an API Client that will allow you basic interaction, data retrieval, and account updating with your __[Bloc](https://www.bloc.io)__ account.  Kele is designed for Bloc students, and will require a valid user ID and password to access much of it's functionality.  For Kele's specific methods and unique abilities, students can reference the __Kele API Client__ project.
--- lib/kele.rb
+++ lib/kele.rb
@@ -41,7 +41,15 @@ class Kele
     response = self.class.post("https://www.bloc.io/api/v1/messages", body: { "sender" => sender_email, "recipient_id" => recipient_id, "subject" => subject, "stripped-text" => stripped_text }, headers: { "authorization" => @auth_token })
   end
 
-  def create_submission(checkpoint_id, enrollment_id)
-    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", body: { "checkpoint_id" => checkpoint_id, "enrollment_id" => enrollment_id }, headers: { "authorization" => @auth_token })
+  def create_submission(checkpoint_id, enrollment_id, assignment_branch = nil, assignment_commit_link = nil, comment = nil)
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions",
+    body: {
+      checkpoint_id: checkpoint_id,
+      enrollment_id: enrollment_id,
+      assignment_branch: assignment_branch,
+      assignment_commit_link: assignment_commit_link,
+      comment: comment
+      },
+    headers: { "authorization" => @auth_token })
   end
 end
